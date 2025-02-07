4f8d44a19a3d8320f86f70cfdcfdc2b8e7eb31a4
Robbie Booth CS2021 <robbie.booth.2021@uni.strath.ac.uk>
2025-02-07 13:42:33 +0000

    Added jwt tokens filtering to now communicate with remix server securely

---
+++ src/main/java/org/robbie/modulareducationenvironment/authentication/SecurityConfig.java
@@ -0,0 +1,38 @@
+package org.robbie.modulareducationenvironment.authentication;
+
+import org.robbie.modulareducationenvironment.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //JWT secret key
+    @Value("${app.jwt.verifier.key}")
+    private String SECRET;
+
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    JwtAuthenticationFilter jwtFilter = new JwtAuthenticationFilter(SECRET);
+    http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth ->
+//                    auth.requestMatchers("/settings").hasRole("user") // Spring will check for "ROLE_EDUCATOR"
+                            auth.requestMatchers("/error").permitAll().anyRequest().authenticated())
+            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .anonymous(anonymous -> anonymous.disable()) // Disable anonymous authentication because its causing true jwt to get over ridden!!!
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+    return http.build();
+}
+}
+
+
